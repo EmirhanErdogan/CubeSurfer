2d5610d6b0d26201c855321b2ab6206af70418e9
Emirhan Erdoğan <89732822+EmirhanErdogan@users.noreply.github.com>
2022-03-23 22:08:33 +0300

    + Edit : Movement script and Input script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Vector3 goright;
+    //Vector3 goleft;
+    //void Update()
+    //{
+    //    goright = new Vector3(-5.5f, transform.position.y, transform.position.z);
+    //    goleft = new Vector3(5.5f, transform.position.y, transform.position.z);
+    //    if (InputController.Instance.zPosition == -1)//goleft2
+    //    {
+    //        transform.position = Vector3.Lerp(transform.position, goleft, 3 * Time.deltaTime);
+    //    }
+    //    else if (InputController.Instance.zPosition == 1)//goright-2
+    //    {
+    //        transform.position = Vector3.Lerp(transform.position, goright, 3 * Time.deltaTime);
+    //    }
+    //}
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float playerBorder = 5.5f;
+    private Touch Touch => Input.GetTouch(0);
+    private Vector3 Pos
+    {
+        get => transform.position;
+        set => transform.position = value;              
+    }
+    private Vector3 movedPos;
+    private void Update()
+    {
+        UpdateInit();
+    }
+    private void UpdateInit()
+    {
+        PlayerVerticalMovement();
+        PlayerHorizantalMovement();
+        SetBorder();     
+    }
+    private void PlayerVerticalMovement()
+    {
+        transform.Translate(new Vector3(0, 0,verticalSpeed*Time.deltaTime));
+    }
+    private void PlayerHorizantalMovement()
+    {
+        if (Input.touchCount <= 0) return;
+
+        if (Touch.phase==TouchPhase.Moved)
+        {
+            movedPos = new Vector3(Pos.x + Touch.deltaPosition.x * (Time.deltaTime * horizontalSpeed), Pos.y, Pos.z);
+            Pos = movedPos;
+        }
+    }
+    private void SetBorder()
+    {
+        if (Pos.x<-playerBorder)
+        {
+            Pos = new Vector3(-playerBorder, Pos.y, Pos.z);
+        }
+        if (Pos.x>playerBorder)
+        {
+            Pos = new Vector3(playerBorder, Pos.y, Pos.z);
+        }
+    }
+
+
+    
+    
+
+
+}
